4edddee570566864876a3d84a5cfd7af31613617
Pablo Buisson <pablo.buisson@gmail.com>
2024-06-18 12:12:13 +0200

    refactor: improve form validation

--- personas-frontend/src/app/actions/project-actions.tsx
+++ personas-frontend/src/app/actions/project-actions.tsx
@@ -1,25 +1,47 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { ProjectDto } from "../api";
+import { ProjectDto, TagDto } from "../api";
 import { createProject } from "../api/endpoints";
 
 export async function handleCreateProject(formData: FormData) {
   const rawFormData = {
     title: formData.get("title"),
     description: formData.get("description"),
-    tag: formData.get("tag"),
+    tags: formData.get("tags"),
   };
+
+  let errors = [];
+
+  if (!rawFormData.title || rawFormData.title.toString().trim().length === 0) {
+    errors.push("Title is required.");
+  }
+
+  if (
+    !rawFormData.description ||
+    rawFormData.description.toString().trim().length === 0
+  ) {
+    errors.push("Description is required.");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  let tagsDTO: TagDto[] = [];
+
+  if (rawFormData.tags) {
+    tagsDTO = rawFormData.tags
+      .toString()
+      .split(/[,\;.\\_\-\/|\s]+/)
+      .map((tagValue) => ({ label: tagValue.trim() }));
+  }
 
   // TODO validation
   const newProject: ProjectDto = {
     name: rawFormData.title as string,
     description: rawFormData.description as string,
-    tags: [
-      {
-        label: rawFormData.tag as string,
-      },
-    ],
+    tags: tagsDTO,
   };
 
   const response: ProjectDto = await createProject(newProject);
--- personas-frontend/src/components/forms/CreateProjectForm.tsx
+++ personas-frontend/src/components/forms/CreateProjectForm.tsx
@@ -1,30 +1,26 @@
 import { handleCreateProject } from "@/app/actions/project-actions";
 
+import InputWithLabel from "./InputWithLabel";
+import ButtonPrimary from "../buttons/ButtonPrimary";
+
 export default function CreateProjectForm() {
   return (
     <form action={handleCreateProject} className="flex flex-col gap-8">
-      <div className="bg-gray-300 relative rounded-full w-28 h-28">
+      <section className="bg-gray-300 relative rounded-full w-28 h-28">
         <button type="button" aria-label="Change emoticon">
           <div className="bg-gray-50 absolute bottom-0 right-0 rounded-full w-12 h-12"></div>
         </button>
-      </div>
-      <input
-        id="title"
-        name="title"
-        className="text-5xl font-extrabold"
-        value="New project"
-      />
-      <textarea
-        id="description"
-        name="description"
-        className="text-xl font-medium"
-        value="Description of the project"
-      />
-      <div>
-        <input id="tag" name="tag"></input>
-        <button type="button">Add a tag</button>
-      </div>
-      <button type="submit">Create project</button>
+      </section>
+      <section className="flex flex-col gap-8">
+        <InputWithLabel label="Name of the project" inputId="title" />
+        <InputWithLabel
+          label="Describe the project"
+          inputId="description"
+          withLongText={true}
+        />
+        <InputWithLabel label="Tags" inputId="tags" />
+      </section>
+      <ButtonPrimary type="submit" label="Create project" />
     </form>
   );
 }
