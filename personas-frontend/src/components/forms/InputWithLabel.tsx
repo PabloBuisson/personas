019f2f2a3a6cb2cdd4e853f2fdf9834389b915c9
Pablo Buisson <pablo.buisson@gmail.com>
2024-06-19 18:30:50 +0200

    refactor(InputWithLabel): add information message

--- personas-frontend/src/components/forms/InputWithLabel.tsx
+++ personas-frontend/src/components/forms/InputWithLabel.tsx
@@ -3,16 +3,21 @@ import LabelForm from "./LabelForm";
 export default function InputWithLabel({
   label,
   inputId,
+  informationMessage,
   withLongText = false,
   ...props
 }: {
   label: string;
   inputId: string;
+  informationMessage?: string;
   withLongText?: boolean;
 }) {
   return (
     <div className="flex flex-col gap-4">
       <LabelForm label={label} inputId={inputId} />
+      {informationMessage && (
+        <p className="p-4 bg-gray-200 rounded-sm text-sm">{informationMessage}</p>
+      )}
       {withLongText ? (
         <textarea
           {...props}
