d191597fd877b2790ef682251198901012faaeef
Pablo Buisson <pablo.buisson@gmail.com>
2024-06-27 12:15:33 +0200

    feat: add edit page for persona

---
+++ personas-frontend/src/app/personas/[personaId]/edit/page.tsx
@@ -0,0 +1,20 @@
+import { PersonaDto } from "@/app/api";
+import { getPersonaById } from "@/app/api/endpoints";
+import EditPersonaForm from "@/components/forms/EditPersonaForm";
+
+export default async function EditPersona({
+  params,
+}: {
+  params: { personaId: string };
+}) {
+  const personaId = params.personaId;
+
+  // TODO get cached Project ?
+  let persona: PersonaDto = await getPersonaById(personaId);
+
+    return (
+      <main className="p-16 flex flex-col gap-8">
+        <EditPersonaForm persona={persona} />
+      </main>
+    );
+}
