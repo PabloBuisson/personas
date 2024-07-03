226c375a8b7fa23d625ef1c155d21a5c500ff034
Pablo Buisson <pablo.buisson@gmail.com>
2024-07-03 15:45:19 +0200

    refactor(frontend): add custom colors

--- personas-frontend/tailwind.config.ts
+++ personas-frontend/tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from "tailwindcss";
 
+const colors = require("tailwindcss/colors");
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +9,36 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      red: colors.red,
+      orange: {
+        50: "hsl(var(--orange-50))",
+        100: "hsl(var(--orange-100))",
+        900: "hsl(var(--orange-900))",
+      },
+      darkorange: {
+        500: "hsl(var(--dark-orange-500))",
+        600: "hsl(var(--dark-orange-600))",
+      },
+      pink: {
+        100: "hs(var(--pink-100))",
+        200: "hsl(var(--pink-200))",
+        500: "hsl(var(--pink-500))",
+        900: "hsl(var(--pink-900))",
+      },
+      purple: {
+        100: "hsl(var(--purple-100))",
+        200: "hs(var(--purple-200))",
+        600: "hs(var(--purple-600))",
+        800: "hsl(var(--purple-800))",
+        900: "hsl(var(--purple-900))",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
