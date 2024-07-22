31e713098fe44f45c4e7eba2f21618b6642a7bf9
Pablo Buisson <pablo.buisson@gmail.com>
2024-07-22 11:01:47 +0200

    refactor(nputWithHiddenLabel): delete error message on input

--- personas-frontend/src/components/forms/common/InputWithHiddenLabel.tsx
+++ personas-frontend/src/components/forms/common/InputWithHiddenLabel.tsx
@@ -1,7 +1,14 @@
 import AppIcon from "@/components/UI/AppIcon";
 import LabelForm from "./LabelForm";
+import { useState } from "react";
 
 type InputWithHiddenLabelProps = InputWithLabelProps | TextAreaWithLabelProps;
+
+type ErrorState = {
+  message?: string;
+  hasBeenChecked?: boolean;
+  hasBeenCleared?: boolean;
+};
 
 export default function InputWithHiddenLabel({
   label,
@@ -11,6 +18,30 @@ export default function InputWithHiddenLabel({
   withLongText,
   ...props
 }: InputWithHiddenLabelProps) {
+  const [errorState, setErrorState] = useState<ErrorState>();
+
+  function clearErrorMessage(event: any) {
+    if (errorMessage && event.target.value) {
+      setErrorState((previousState) => ({
+        ...previousState,
+        message: undefined,
+        hasBeenCleared: true,
+      }));
+    }
+  }
+
+  if (
+    errorMessage &&
+    !errorState?.message &&
+    !errorState?.hasBeenCleared &&
+    !errorState?.hasBeenChecked
+  ) {
+    setErrorState({
+      message: errorMessage,
+      hasBeenChecked: true,
+    });
+  }
+
   return (
     <div className="flex flex-col gap-4">
       {informationMessage && (
@@ -33,15 +64,16 @@ export default function InputWithHiddenLabel({
             style={{ width: "100%" }}
             name={inputId}
             {...(props as React.ComponentPropsWithoutRef<"input">)}
+            onInput={(e) => clearErrorMessage(e)}
           />
         )}
-        {errorMessage && (
+        {errorState?.message && (
           <div className="flex items-center gap-2 bg-red-800 py-2 pl-2 pr-4 absolute top-0 translate-y-[-50%] left-3 rounded-sm">
             <AppIcon
               icon="mdi:alert-circle-outline"
               className="text-lg text-red-300"
             />
-            <p className=" text-red-50 text-sm">{errorMessage}</p>
+            <p className=" text-red-50 text-sm">{errorState.message}</p>
           </div>
         )}
       </div>
